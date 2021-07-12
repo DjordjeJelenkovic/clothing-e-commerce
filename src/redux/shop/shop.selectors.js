82f01efcbc8cf9a42edd6a8fa236da3cf864c156
Djordje Jelenkovic <ledjo.jelenkovic@gmail.com>
2021-07-12 21:14:23 +0200

    1. Data Normalization + Collection Page

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-}
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -26,8 +18,8 @@ export const selectCollections = createSelector(
 //     collections => collections[collectionUrlParam]
 //   );
 
-  export const selectCollection = collectionUrlParam =>
+export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    collections => collections[collectionUrlParam]
   );
