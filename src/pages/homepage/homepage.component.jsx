14049667874ae9b0f75e1416da4b8b38070b0f66
Djordje Jelenkovic <ledjo.jelenkovic@gmail.com>
2021-07-16 13:42:35 +0200

    3. styled-components In Our App

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-import './homepage.styles.scss';
+import { HomePageContainer } from './homepage.styles';
 
-const HomePage = ({history}) => (
-  <div className='homepage'>
-    <Directory history={history}/>
-  </div>
+const HomePage = () => (
+  <HomePageContainer>
+    <Directory/>
+  </HomePageContainer>
 );
 
 export default HomePage;
