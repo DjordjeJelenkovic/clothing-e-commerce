a45a655563b6fa62c4768d6ab521e0057d87cf20
Djordje Jelenkovic <ledjo.jelenkovic@gmail.com>
2021-07-29 12:12:28 +0200

    18. 3. Redux Thunk

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-import {
-  firestore,
-  convertCollectionsSnapshotToMap
-} from '../../firebase/firebase.utils';
+// import {
+//   firestore,
+//   convertCollectionsSnapshotToMap
+// } from '../../firebase/firebase.utils';
 
-import { updateCollections } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
 
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
@@ -18,53 +20,60 @@ const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
-  state = {
-    loading: true
-  };
+  // state = {
+  //   loading: true
+  // };
 
-  unsubscribeFromSnapshot = null;
+  // unsubscribeFromSnapshot = null;
 
   componentDidMount() {
-    console.log("Zdravo")
-    const { updateCollections } = this.props;
-    const collectionRef = firestore.collection('collections');
+    // console.log("Zdravo")
+    // const { updateCollections } = this.props;
+    // const collectionRef = firestore.collection('collections');
 
-    // fetch('https://firestore.googleapis.com/v1/projects/crwn-db-72e2a/databases/(default)/documents/collections')
-    // .then(response => response.json())
-    // .then(collections => console.log(collections));
+    // // fetch('https://firestore.googleapis.com/v1/projects/crwn-db-72e2a/databases/(default)/documents/collections')
+    // // .then(response => response.json())
+    // // .then(collections => console.log(collections));
 
-    collectionRef.get().then(snapshot => {
-      // console.log(snapshot)
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      // console.log(collectionsMap);
-      updateCollections(collectionsMap);
-      this.setState({ loading: false });
-    })
+    // collectionRef.get().then(snapshot => {
+    //   // console.log(snapshot)
+    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //   // console.log(collectionsMap);
+    //   updateCollections(collectionsMap);
+    //   this.setState({ loading: false });
+    // });
+    const { fetchCollectionsStartAsync } = this.props;
+    fetchCollectionsStartAsync();
   }
 
   render() {
-    const {match} = this.props;
-    const { loading } = this.state;
+    const {match, isCollectionFetching} = this.props;
+    // const { loading } = this.state;
 
     return (
       <div className='shop-page'>
         <Route exact path={`${match.path}`} render={props => (
-            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+            <CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props} />
           )} />
         <Route path={`${match.path}/:collectionId`} render={props => (
-            <CollectionPageWithSpinner isLoading={loading} {...props} />
+            <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props} />
           )} />
      </div>
     )
   }
 };
 
+const mapStatetoProps = createStructuredSelector({
+  isCollectionFetching: selectIsCollectionFetching
+});
+
 const mapDispatchToProps = dispatch => ({
-  updateCollections: collectionsMap =>
-    dispatch(updateCollections(collectionsMap))
+  // updateCollections: collectionsMap =>
+  //   dispatch(updateCollections(collectionsMap))
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 });
 
 export default connect(
-  null,
+  mapStatetoProps,
   mapDispatchToProps
 )(ShopPage);
