889eb34ba1a55e165750aa09354e5b70f8a9aa03
Djordje Jelenkovic <ledjo.jelenkovic@gmail.com>
2021-08-28 17:58:31 +0200

    15. Sign Out With Sagas

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -23,12 +23,15 @@ export const checkUserSession = () => ({
   type: UserActionTypes.CHECK_USER_SESSION
 }); 
 
-// export const emailSignInSuccess = user => ({
-//   type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
-//   payload: user
-// });
+export const signOutStart = () => ({
+  type: UserActionTypes.SIGN_OUT_START
+});
 
-// export const emailSignInFailure = error => ({
-//   type: UserActionTypes.EMAIL_SIGN_IN_FAILURE,
-//   payload: error
-// });
+export const signOutSuccess = () => ({
+  type: UserActionTypes.SIGN_OUT_SUCCESS
+});
+
+export const signOutFailure = error => ({
+  type: UserActionTypes.SIGN_OUT_FAILURE,
+  payload: error
+});
